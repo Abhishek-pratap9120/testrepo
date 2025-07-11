2e92036e8baf48516bb2a0a43562b402b78eb87a
Abhishek-pratap9120 <abhi911387@gmail.com>
2025-07-11 18:56:16 +0530

    added rg with variable

--- main.tf
+++ main.tf
@@ -3,3 +3,13 @@ resource "azurerm_resource_group" "rg-test-repo" {
     location = "west us"
   
 }
+resource "azurerm_resource_group" "rg-ticket121" {
+    name = var.rg_name
+    location = var.rg_location
+}
+variable "rg_name" {
+    default = "rg121"
+}
+variable "rg_location" {
+    default = "central india"
+}
